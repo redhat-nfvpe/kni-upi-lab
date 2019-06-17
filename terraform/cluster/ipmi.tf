d8a2601e095f07a537ccb1a0a70aee866fa1b3d6
Yolanda Robla <yroblamo@redhat.com>
2019-06-17 14:26:46 +0200

    Add fix of master power

--- terraform/cluster/ipmi.tf
+++ terraform/cluster/ipmi.tf
@@ -12,7 +12,7 @@ resource "null_resource" "ipmi_master_cleanup" {
     provisioner "local-exec" {
         when = "destroy"
         command = <<EOT
-          ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} power off;
+          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power off;
 EOT
     }
 }
