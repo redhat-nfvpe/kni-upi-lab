fd7c18548ba041a44a2c50016aa1ed1cc6116ff3
Yolanda Robla <yroblamo@redhat.com>
2019-06-17 14:20:59 +0200

    Add power on commands after power cycle

--- terraform/cluster/ipmi.tf
+++ terraform/cluster/ipmi.tf
@@ -2,7 +2,8 @@ resource "null_resource" "ipmi_master" {
     provisioner "local-exec" {
         command = <<EOT
           ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power cycle;
+          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.worker_ipmi_host} -U ${var.worker_ipmi_user} -P
+${var.worker_ipmi_pass} power cycle;
 EOT
     }
 }
