919dbbc5eb3597e3da0a383fe676065eb179ca8b
Yolanda Robla <yroblamo@redhat.com>
2019-06-17 14:46:11 +0200

    Fix newline addition

--- terraform/cluster/ipmi.tf
+++ terraform/cluster/ipmi.tf
@@ -2,8 +2,7 @@ resource "null_resource" "ipmi_master" {
     provisioner "local-exec" {
         command = <<EOT
           ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P
-${var.master_ipmi_pass} power on;
+          ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.master_ipmi_host} -U ${var.master_ipmi_user} -P ${var.master_ipmi_pass} power on;
 EOT
     }
 }
