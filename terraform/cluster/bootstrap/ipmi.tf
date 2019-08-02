7f5086a02d58819ccafce802f2ea3cf5cee152f2
Yolanda Robla <yroblamo@redhat.com>
2019-08-02 12:30:40 +0200

    Add fixes in IPMI for bootstrap being a host

--- terraform/cluster/bootstrap/ipmi.tf
+++ terraform/cluster/bootstrap/ipmi.tf
@@ -1,8 +1,7 @@
 resource "null_resource" "ipmi_bootstrap" {
     provisioner "local-exec" {
         command = <<EOT
-          rm -f /var/lib/libvirt/images/bootstrap.img || true
-          qemu-img create -f qcow2 /var/lib/libvirt/images/bootstrap.img 800G
+          ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} chassis bootdev pxe;
           ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} power cycle || ipmitool -I lanplus -H ${var.bootstrap_ipmi_host} -U ${var.bootstrap_ipmi_user} -P ${var.bootstrap_ipmi_pass} power on;
 EOT
     }
