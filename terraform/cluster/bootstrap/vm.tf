08f744c0012c9ce88cb0157110dcdba1d4a494b8
Yolanda Robla <yroblamo@redhat.com>
2019-08-05 16:07:33 +0200

    Remove destroy command as we use undefine

--- terraform/cluster/bootstrap/vm.tf
+++ terraform/cluster/bootstrap/vm.tf
@@ -36,7 +36,6 @@ resource "null_resource" "vm_bootstrap_destroy" {
     provisioner "local-exec" {
         when = "destroy"
         command = <<EOT
-virsh destroy ${var.cluster_id}-bootstrap
 virsh undefine ${var.cluster_id}-bootstrap
 EOT
     }
