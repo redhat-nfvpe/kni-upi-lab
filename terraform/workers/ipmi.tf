3d987927c0c71f1d496cc272918cbec85d989844
Andrew Bays <andrew.bays@gmail.com>
2019-11-14 07:31:55 -0500

    Ignore redfishtool return code for workers

--- terraform/workers/ipmi.tf
+++ terraform/workers/ipmi.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "ipmi_worker" {
     count = var.enable_redfish ? 0 : var.worker_count
-    
+
     provisioner "local-exec" {
         command = <<EOT
-          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} power cycle || ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} power on;
+          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} chassis bootdev pxe || true;
+          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} power cycle || ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} power on || true;
 EOT
     }
 }
@@ -15,7 +15,7 @@ resource "null_resource" "ipmi_worker_clenup" {
     provisioner "local-exec" {
         when = "destroy"
         command = <<EOT
-          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} power off;
+          ipmitool -I lanplus -H ${var.worker_nodes[count.index]["ipmi_host"]} -U ${var.worker_nodes[count.index]["ipmi_user"]} -P ${var.worker_nodes[count.index]["ipmi_pass"]} power off || true;
 EOT
     }
 }
