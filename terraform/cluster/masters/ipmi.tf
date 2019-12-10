6e2825320177eae5f07b2883b1dd8f11db9128ab
Yolanda Robla <yroblamo@redhat.com>
2019-12-10 12:36:38 +0100

    Add fixes to use port for ipmi if it exists

--- terraform/cluster/masters/ipmi.tf
+++ terraform/cluster/masters/ipmi.tf
@@ -2,8 +2,8 @@ resource "null_resource" "ipmi_master" {
     count = var.master_count
     provisioner "local-exec" {
         command = <<EOT
-          ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} chassis bootdev pxe;
-          ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power cycle || ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power on;
+          ipmitool -I lanplus -H ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),0)} %{if element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)!=""}-p ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)}%{ endif } -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} chassis bootdev pxe;
+          ipmitool -I lanplus -H ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),0)} %{if element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)!=""}-p ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)}%{ endif } -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power cycle || ipmitool -I lanplus -H ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),0)} %{if element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)!=""}-p ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)}%{ endif } -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power on;
 EOT
     }
 }
@@ -13,7 +13,7 @@ resource "null_resource" "ipmi_master_cleanup" {
     provisioner "local-exec" {
         when = "destroy"
         command = <<EOT
-          ipmitool -I lanplus -H ${var.master_nodes[count.index]["ipmi_host"]} -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power off;
+          ipmitool -I lanplus -H ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),0)} %{if element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)!=""}-p ${element(split(":", var.master_nodes[count.index]["ipmi_host"]),1)}%{ endif } -U ${var.master_nodes[count.index]["ipmi_user"]} -P ${var.master_nodes[count.index]["ipmi_pass"]} power off;
 EOT
     }
 }
