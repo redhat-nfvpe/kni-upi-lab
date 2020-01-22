42991315e435e61d4ba2487a1584a5002f6789c3
Andrew Bays <andrew.bays@gmail.com>
2020-01-22 07:35:28 -0500

    Fix gen_ignition script standalone manifest copy

--- scripts/gen_ignition.sh
+++ scripts/gen_ignition.sh
@@ -102,7 +102,7 @@ patch_manifest() {
 
     if [[ $standalone =~ true ]]; then
         cp_manifest "$PROJECT_DIR/cluster/standalone/openshift" "$ocp_dir/openshift"
-        cp_manifest "$PROJECT_DIR/cluster/standalone/manifest" "$ocp_dir/manifest"
+        cp_manifest "$PROJECT_DIR/cluster/standalone/manifest" "$ocp_dir/manifests"
 
 	# TODO remove this workaround for 4.4 deployment
 	if [[ $OPENSHIFT_RHCOS_MAJOR_REL == "4.4" ]]; then
