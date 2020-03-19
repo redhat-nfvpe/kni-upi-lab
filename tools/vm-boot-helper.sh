3b847930a91e9cca62a69c05aca39898336ebb63
Andrew Bays <andrew.bays@gmail.com>
2020-03-19 08:18:46 -0400

    Remove comments from vm-boot-helper

--- tools/vm-boot-helper.sh
+++ tools/vm-boot-helper.sh
@@ -2,15 +2,6 @@
 
 CLUSTER_NAME="kni-upi-lab"
 
-# THIS_PID="$(pgrep -f $0)"
-
-# Kill all other instances of this script
-# for i in $(ps ax | grep "$0" | grep -v grep | awk {'print $1'}); do
-#     if [[ "$i" != "$THIS_PID" ]]; then
-#         sudo kill -9 $i 2>/dev/null
-#     fi
-# done
-
 while true; do
     vms_present=$(sudo virsh list --all | grep $CLUSTER_NAME)
 
