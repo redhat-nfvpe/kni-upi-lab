ae29d5300a16d87513738e2bb67a43f1e6ff9543
Andrew Bays <andrew.bays@gmail.com>
2020-03-23 16:17:57 -0400

    Permanent ipv4 forwarding

--- scripts/gen_iptables.sh
+++ scripts/gen_iptables.sh
@@ -62,5 +62,10 @@ if [[ -n "$REJECT_RULE2" ]]; then
 fi
 
 # enable ipv4 forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -p
+IP_FORWARD=$(sudo grep "net.ipv4.ip_forward = 1" /etc/sysctl.conf | grep -v "#")
+
+if [[ -z "$IP_FORWARD" ]]; then
+sudo tee -a /etc/sysctl.conf > /dev/null << EOF
+net.ipv4.ip_forward = 1
+EOF
+fi
